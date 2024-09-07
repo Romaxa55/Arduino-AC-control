af808f9dfa22938363b3bd969156df13728ca8c3
Roman SHamagin <roman.shamagin@7vpn.com>
2024-09-07 21:34:14 +0300

    init

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,31 +1,26 @@
-#include "LED.h"
-#include "IRReceiver.h"
+#include "MelodyPlayer.h"  // Подключаем файл класса
 
-LED led1(13);          // Экземпляр класса LED для пина 13
-IRReceiver irReceiver(11); // Экземпляр класса IRReceiver для пина 11 (подключите ИК-приемник к этому пину)
+// Задаем мелодию и длительности нот
+int melody[] = {
+    NOTE_E7, NOTE_E7, 0, NOTE_E7,
+    0, NOTE_C8, NOTE_E7, 0,
+    NOTE_G7, 0, 0, 0,
+    NOTE_G7, 0, 0, 0,
+    NOTE_A7, 0, NOTE_A7, 0,
+    NOTE_G7, NOTE_F7, NOTE_F7, NOTE_F7, NOTE_F7,
+};
+
+int noteDurations[] = {
+    4, 4, 8, 4, 8, 4, 8, 4, 8, 4, 8, 8, 8, 4, 8, 4, 8, 8, 4, 8, 4, 8, 4, 8
+};
+
+MelodyPlayer player(LED_BUILTIN, melody, noteDurations, sizeof(melody) / sizeof(melody[0]));
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println(F("LED control program with IR started."));
-    irReceiver.begin(); // Инициализируем ИК-приемник
+    // Пусто, так как все уже инициализировано в классе
 }
 
 void loop() {
-    uint32_t receivedCommand = irReceiver.receive(); // Получаем команду с пульта
-
-    if (receivedCommand) {
-        Serial.print(F("Received IR command: "));
-        Serial.println(receivedCommand, HEX); // Печатаем команду в шестнадцатеричном формате
-
-        // Простая обработка команд - можно добавить свои условия
-        if (receivedCommand == 0x20DF10EF) { // Пример кода для включения светодиода
-            led1.on();
-        } else if (receivedCommand == 0x20DF906F) { // Пример кода для выключения светодиода
-            led1.off();
-        } else {
-            led1.toggle(); // Переключаем состояние светодиода по любой другой команде
-        }
-    }
-
-    delay(100); // Небольшая задержка для стабилизации
+    player.play();
+    delay(1000);  // Задержка перед повторным воспроизведением
 }
