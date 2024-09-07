d11bcc355a687017135e8a087ad0d6eb3fac4896
Roman SHamagin <roman.shamagin@7vpn.com>
2024-09-07 21:22:32 +0300

    init

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,24 +1,31 @@
 #include "LED.h"
+#include "IRReceiver.h"
 
-LED led1(13); // Экземпляр класса LED для пина 13
+LED led1(13);          // Экземпляр класса LED для пина 13
+IRReceiver irReceiver(11); // Экземпляр класса IRReceiver для пина 11 (подключите ИК-приемник к этому пину)
 
 void setup() {
     Serial.begin(9600);
-    Serial.println(F("LED control program started.")); // Используем F() для строк
+    Serial.println(F("LED control program with IR started."));
+    irReceiver.begin(); // Инициализируем ИК-приемник
 }
 
 void loop() {
-    Serial.println(F("LED is ON"));
-    led1.on();
-    delay(1000);
+    uint32_t receivedCommand = irReceiver.receive(); // Получаем команду с пульта
 
-    Serial.println(F("LED is OFF"));
-    led1.off();
-    delay(1000);
+    if (receivedCommand) {
+        Serial.print(F("Received IR command: "));
+        Serial.println(receivedCommand, HEX); // Печатаем команду в шестнадцатеричном формате
 
-    Serial.println(F("LED is blinking"));
-    led1.blink(500);
+        // Простая обработка команд - можно добавить свои условия
+        if (receivedCommand == 0x20DF10EF) { // Пример кода для включения светодиода
+            led1.on();
+        } else if (receivedCommand == 0x20DF906F) { // Пример кода для выключения светодиода
+            led1.off();
+        } else {
+            led1.toggle(); // Переключаем состояние светодиода по любой другой команде
+        }
+    }
 
-    led1.toggle();
-    delay(1000);
+    delay(100); // Небольшая задержка для стабилизации
 }
